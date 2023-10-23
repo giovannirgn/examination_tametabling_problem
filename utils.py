2942c7dedeb4efa802fe424c5dc9d1567765a700
giovannirgn <giovanni.regano@gmail.com>
2023-10-23 17:25:26 +0200

    update final model

--- utils.py
+++ utils.py
@@ -4,27 +4,6 @@ def comb_exames(Exames):
     return combinations(Exames,2)
 
 
-def distances(time_slots):
-    return [i for i in range(time_slots)]
-
-
-def distance_param_cost(distances_list,max_distance_penality):
-
-    dist_param = dict()
-
-    for dist in distances_list:
-
-        if dist > max_distance_penality:
-
-            dist_param[dist] = max_distance_penality
-
-        else:
-
-            dist_param[dist] = dist
-
-    return dist_param
-
-
 def distance_2(dist_penality):
 
     return [i for i in range(1,dist_penality+1)]
